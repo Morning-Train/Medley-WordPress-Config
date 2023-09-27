e52a3901d06fcc70e5dd7e9ff9dfcd05050b1b44
Mathias Munk <mh@morningtrain.dk>
2023-09-27 14:44:46 +0200

    Add basic structure

---
+++ src/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MorningMedley\Config;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+
+    }
+
+}
